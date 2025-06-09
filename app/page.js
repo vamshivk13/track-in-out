068bed4e53d65faff6ec7d242e346dd0534754d6
vamshi thatikonda <thatikondavamshi13@gmail.com>
2025-06-09 17:04:37 +0530

    action buttons added

--- app/page.js
+++ app/page.js
@@ -17,6 +17,9 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs from "dayjs";
 import { useState } from "react";
 
+import { X } from "lucide-react";
+import { Pencil } from "lucide-react";
+
 export default function Home() {
   const currentDate = new Date();
   const [selected, setSelected] = useState(currentDate);
@@ -26,7 +29,8 @@ export default function Home() {
     selected && selected?.toLocaleString("default", { weekday: "long" });
   const day = selected?.getDate();
   const year = selected?.getFullYear();
-  // console.log(typeof selected, selected.toLocaleString())
+
+  const isEnteredIn = false;
   return (
     <div className="flex gap-4 py-10 flex-col md:flex-row sm:flex-col md:h-[100%] h-[calc(100%-30px)]  ">
       <Calendar
@@ -45,29 +49,57 @@ export default function Home() {
         </CardHeader>
         <CardContent className={"h-[100%]"}></CardContent>
         <CardAction className={"w-full"}>
-          {/* <ul className="flex justify-between p-10">
-          <li><Button variant="outline" >Enter</Button></li>
-          <li><Button variant={"outline"}>Exit</Button></li>
-           </ul> */}
-
-          <ul className="flex gap-2 justify-between px-4">
-            <li className="border my-auto rounded-2xl p-3  flex flex-col md:flex-row gap-3 justify-center align-middle">
-              <Badge variant="default" className="md:w-min w-full">
-                Time in
-              </Badge>
-              <p>{currentDate.toLocaleTimeString()}</p>
+          <ul className="flex gap-2 px-4 items-center">
+            <li className="flex-1">
+              {isEnteredIn ? (
+                <Button variant="outline">Exit</Button>
+              ) : (
+                <div className="border my-auto rounded-2xl p-3  flex flex-col md:flex-row gap-3 md:justify-between ">
+                  <Badge
+                    variant="default"
+                    className="md:w-min w-full text-[1rem]"
+                  >
+                    Time in
+                  </Badge>
+                  <div className="self-center flex items-center gap-2">
+                    <div className="flex-1">
+                      {currentDate.toLocaleTimeString()}
+                    </div>
+                    <Button variant={"outlined"} className="size-4">
+                      <Pencil />
+                    </Button>
+                  </div>
+                </div>
+              )}
             </li>
             {/* <li className="rounded-2xl overflow-hidden">
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
             <StaticTimePicker defaultValue={dayjs('2022-04-17T15:30')} />
             </LocalizationProvider>
               </li> */}
-            <li className="border my-auto rounded-2xl p-3  flex flex-col md:flex-row gap-3 justify-center align-middle">
-              <Badge variant="default" className="md:w-min w-full">
-                Time out
-              </Badge>
-              <p>{currentDate.toLocaleTimeString()}</p>
+            <li className="flex-1">
+              {isEnteredIn ? (
+                <Button variant="outline">Exit</Button>
+              ) : (
+                <div className="border my-auto rounded-2xl p-3  flex flex-col md:flex-row gap-3 md:justify-between ">
+                  <Badge
+                    variant="default"
+                    className="md:w-min w-full text-[1rem]"
+                  >
+                    Time out
+                  </Badge>
+                  <div className="self-center flex items-center gap-2">
+                    {currentDate.toLocaleTimeString()}
+                    <Button variant={"outlined"} className="size-4">
+                      <Pencil />
+                    </Button>
+                  </div>
+                </div>
+              )}
             </li>
+            <Button variant={"outlined"} className="size-4">
+              <X />
+            </Button>
           </ul>
         </CardAction>
       </Card>
