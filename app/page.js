6abaf925a76162d9d80a8722a3d88c77560009e3
vamshi thatikonda <thatikondavamshi13@gmail.com>
2025-06-10 17:31:29 +0530

    update ui

--- app/page.js
+++ app/page.js
@@ -29,21 +29,130 @@ export default function Home() {
     selected && selected?.toLocaleString("default", { weekday: "long" });
   const day = selected?.getDate();
   const year = selected?.getFullYear();
+
+  const [enteredTimeStamp, setEnteredTimeStamp] = useState(null);
+  const [exitTimeStamp, setExitTimeStamp] = useState(null);
+
   const [isEnteredIn, setIsEnteredIn] = useState(false);
   const [isExited, setIsExited] = useState(false);
+  const [workedDays, setWorkedDays] = useState([]);
+  const [bookedDays, setBookedDays] = useState([]);
+  const [inDays, setInDays] = useState([]);
 
   function handleEntryExitReset() {
     setIsEnteredIn(false);
     setIsExited(false);
+    setInDays((prev) =>
+      prev.filter(
+        (day) => day.toLocaleDateString() != selected.toLocaleDateString()
+      )
+    );
+    setBookedDays((prev) =>
+      prev.filter(
+        (day) => day.toLocaleDateString() != selected.toLocaleDateString()
+      )
+    );
+    setWorkedDays((prev) =>
+      prev.filter((days) => days.day != selected.toLocaleDateString())
+    );
   }
 
+  function handleEnteredInState() {
+    const curDate = new Date(getTimeStampFromDate(selected));
+    setEnteredTimeStamp(curDate.toLocaleTimeString());
+    setIsEnteredIn(true);
+    // Handle adding an entry to the database
+    setWorkedDays((prev) => [
+      ...prev,
+      {
+        day: selected.toLocaleDateString(),
+        userId: null,
+        status: "ENTERED",
+        enteredTimeStamp: curDate.toLocaleTimeString(),
+        exitTimeStamp: null,
+        minDuration: null,
+      },
+    ]);
+    setInDays((prev) => [...prev, curDate]);
+  }
+
+  function getTimeStampFromDate(date) {
+    const curTime = new Date();
+    return new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate(),
+      curTime.getHours(),
+      curTime.getMinutes(),
+      curTime.getSeconds()
+    );
+  }
+
+  function handleExitState() {
+    const curDate = new Date(getTimeStampFromDate(selected));
+    setExitTimeStamp(curDate.toLocaleTimeString());
+    setIsExited(true);
+    setWorkedDays((prev) => {
+      return prev.map((curDay) => {
+        console.log("LcoalDate", curDate.toLocaleDateString());
+        if (curDay.day == curDate.toLocaleDateString()) {
+          return {
+            ...curDay,
+            exitTimeStamp: curDate.toLocaleTimeString(),
+            status: "Exited",
+          };
+        } else {
+          return curDay;
+        }
+      });
+    });
+    setInDays((prev) =>
+      prev.filter(
+        (day) => day.toLocaleDateString() != curDate.toLocaleDateString()
+      )
+    );
+    setBookedDays((prev) => [...prev, curDate]);
+  }
+
+  console.log("workedDays", workedDays);
+  console.log("SELECTED", selected);
+  console.log("INDays", inDays);
+  console.log("Booked", bookedDays);
+  function handleSelectedDate(date) {
+    setSelected(date);
+    //get and set timeStamp for enter and exit based on the date
+    const curDayDetails = workedDays.filter(
+      (curDay) => curDay.day == date.toLocaleDateString()
+    )[0];
+    if (curDayDetails) {
+      setIsEnteredIn(!!curDayDetails.enteredTimeStamp);
+      setIsExited(!!curDayDetails.exitTimeStamp);
+      setEnteredTimeStamp(curDayDetails.enteredTimeStamp);
+      setExitTimeStamp(curDayDetails.exitTimeStamp);
+    } else {
+      setIsEnteredIn(false);
+      setIsExited(false);
+      setEnteredTimeStamp(null);
+      setExitTimeStamp(null);
+    }
+  }
   return (
     <div className="flex gap-4 py-3 md:py-8 flex-col md:flex-row sm:flex-col md:h-[100%] h-[calc(100%-60px)] px-0 md:px-3">
       <Calendar
         mode="single"
         selected={selected}
-        onSelect={setSelected}
+        onSelect={handleSelectedDate}
         className={"self-center md:self-baseline"}
+        modifiers={{
+          booked: bookedDays,
+          in: inDays,
+        }}
+        modifiersClassNames={{
+          booked: "[&>button]:!bg-emerald-600",
+          in: "[&>button]:!bg-amber-600",
+          today: "initial",
+        }}
+        // className="rounded-lg border shadow-sm"
       ></Calendar>
       <Card className={"md:h-[calc(100%-30px)] mx-5 flex-1"}>
         <CardHeader>
@@ -55,13 +164,13 @@ export default function Home() {
         </CardHeader>
         <CardContent className={"h-[100%]"}></CardContent>
         <CardAction className={"w-full"}>
-          <ul className="flex gap-2 px-4 items-center">
+          <ul className="flex gap-2 px-4 items-center ">
             <li className="flex-1">
               {!isEnteredIn ? (
                 <Button
                   className="w-full"
                   variant="outline"
-                  onClick={setIsEnteredIn}
+                  onClick={handleEnteredInState}
                 >
                   Enter In
                 </Button>
@@ -74,9 +183,7 @@ export default function Home() {
                     Time in
                   </Badge>
                   <div className="self-center flex items-center gap-2">
-                    <div className="flex-1">
-                      {currentDate.toLocaleTimeString()}
-                    </div>
+                    <div className="flex-1">{enteredTimeStamp}</div>
                     <Button variant={"outlined"} className="size-4">
                       <Pencil />
                     </Button>
@@ -95,7 +202,7 @@ export default function Home() {
                   <Button
                     className="w-full"
                     variant="outline"
-                    onClick={setIsExited}
+                    onClick={handleExitState}
                   >
                     Exit
                   </Button>
@@ -109,7 +216,7 @@ export default function Home() {
                       Time out
                     </Badge>
                     <div className="self-center flex items-center gap-2">
-                      {currentDate.toLocaleTimeString()}
+                      {exitTimeStamp}
                       <Button variant={"outlined"} className="size-4">
                         <Pencil />
                       </Button>
