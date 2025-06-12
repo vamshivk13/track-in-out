607ead1b3a29e0564ce36bb2514951816567c40a
vamshi thatikonda <thatikondavamshi13@gmail.com>
2025-06-12 20:48:11 +0530

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+import nextPWA from "next-pwa";
+
+const withPwa = nextPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPwa(nextConfig);
